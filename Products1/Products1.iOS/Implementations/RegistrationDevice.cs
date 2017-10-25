4fea1595463ac403d7f7cd4aa00a9414b53a237a
Zulu55 <jzuluaga55@gmail.com>
2017-10-25 11:46:13 -0500

    Registration device on iOS

---
+++ Products1/Products1.iOS/Implementations/RegistrationDevice.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Products1.iOS.Implementations.RegistrationDevice))]
+
+namespace Products1.iOS.Implementations
+{
+    using Interfaces;
+    using UIKit;
+    using Foundation;
+
+    public class RegistrationDevice: IRegisterDevice
+    {
+        #region Methods
+        public void RegisterDevice()
+        {
+            if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                        UIUserNotificationType.Alert | 
+                        UIUserNotificationType.Badge | 
+                        UIUserNotificationType.Sound,
+                       new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(
+                    pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationTypes = 
+                    UIRemoteNotificationType.Alert |
+                    UIRemoteNotificationType.Badge | 
+                    UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.
+                             RegisterForRemoteNotificationTypes(notificationTypes);
+            }
+        }
+        #endregion
+    }
+}
